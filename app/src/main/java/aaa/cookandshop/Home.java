4c905d3ab5cf5a714a75be32b62b7ecb9c752000
Misha <misha.kuzn@gmail.com>
2017-08-02 18:40:59 -0500

    vevwav

--- app/src/main/java/aaa/cookandshop/Home.java
+++ app/src/main/java/aaa/cookandshop/Home.java
@@ -1,7 +1,9 @@
 package aaa.cookandshop;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Home extends AppCompatActivity {
 
@@ -9,5 +11,19 @@ public class Home extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+    }
+
+    public void goToShoppingList(View view) {
+
+        Intent intent = new Intent(this, ShoppingList.class);
+
+        startActivity(intent);
+    }
+
+    public void goToRecipes(View view) {
+
+        Intent intent2 = new Intent(this, Recipes.class);
+
+        startActivity(intent2);
     }
 }
