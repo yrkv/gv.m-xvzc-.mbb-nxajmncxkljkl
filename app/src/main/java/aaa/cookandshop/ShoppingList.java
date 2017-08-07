8cc3d31fe8eb8bd52e6fba3a5f185821c1fd16f7
yrkv <yegorushka.kuznetsov@gmail.com>
2017-08-07 16:35:57 -0500

    removed unneeded code, added icon

--- app/src/main/java/aaa/cookandshop/ShoppingList.java
+++ app/src/main/java/aaa/cookandshop/ShoppingList.java
@@ -1,6 +1,7 @@
 package aaa.cookandshop;
 
 import android.content.res.Resources;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.TypedValue;
@@ -18,9 +19,6 @@ import java.util.ArrayList;
 
 public class ShoppingList extends AppCompatActivity {
 
-    ArrayList<CheckBox> listBoxes = new ArrayList<CheckBox>(); //sets up 2 parallel arraylists for boxes and texts
-    ArrayList<EditText> listTexts = new ArrayList<EditText>();
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,63 +26,30 @@ public class ShoppingList extends AppCompatActivity {
     }
 
     public void addItem(View v) {
-        addItem();
+        addItem("");
     }
 
-    public void addItem() {
+    public void addItem(String text) {
+        // get the LinearLayout
         LinearLayout projectsList = (LinearLayout) findViewById(R.id.itemList);
 
-        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams( LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT );
-
-        CheckBox box = new CheckBox(this);
-
-        EditText text = new EditText(this);
-
+        // make a copy of the button
         View button = getLayoutInflater().inflate(R.layout.shopping_list_item, projectsList, false);
 
-        ((EditText)button.findViewById(R.id.itemText)).setText("aaa");
+        // set the text
+        ((EditText)button.findViewById(R.id.itemText)).setText(text);
 
+        // add it to the list
         projectsList.addView(button);
 
+        // TODO: add it to a list of items
 
-        /*
-        set attributes here
-         */
-        Resources r = getResources();
-
-        int px = (int) TypedValue.applyDimension(
-                TypedValue.COMPLEX_UNIT_DIP,
-                20,
-                r.getDisplayMetrics()
-        );
-
-        params.setMargins(0, 0, 0, px);
-
-        box.setLayoutParams(params);
-
-        text.setLayoutParams(params);
-
-//        projectsList.addView(box);
-//        projectsList.addView(text);
-
-        // add the boxes and texts to their respective arraylists
-        listBoxes.add(box);
-        listTexts.add(text);
-
-        CheckBox satView = new CheckBox(this);
-
-        satView.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+        ((CheckBox)button.findViewById(R.id.checkBox))
+                .setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView,boolean isChecked) {
                 //what happens when checkbox is checked.
             }
         });
-    }
-
-    public void recipeAddItem(String name) {
-        addItem();
-
-        EditText editText = (EditText)findViewById(R.id.text);
-        editText.setText(name, TextView.BufferType.EDITABLE);
     }
 }
