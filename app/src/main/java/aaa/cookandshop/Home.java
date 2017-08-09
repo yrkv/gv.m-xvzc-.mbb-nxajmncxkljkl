dc6243c467f82a897b3779acc62825c5a98074f8
yrkv <yegorushka.kuznetsov@gmail.com>
2017-08-09 13:51:26 -0500

    shit

--- app/src/main/java/aaa/cookandshop/Home.java
+++ app/src/main/java/aaa/cookandshop/Home.java
@@ -1,9 +1,11 @@
 package aaa.cookandshop;
 
 import android.content.Intent;
+import android.graphics.Paint;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class Home extends AppCompatActivity {
 
